82b76be17b46f62af4ac4efb023a4378517de97e
AlfiyaPS <alfiyaps2024a@mca.ajce.in>
2023-10-02 19:22:26 +0530

    commit2

--- stayhubapp/urls.py
+++ stayhubapp/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from.import views
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('',views.index,name="home"),
     path('login/',views.loginn,name="login"),
     path('registration/',views.registration,name="registration"),
     path('registerproperty/',views.registerproperty,name="registerproperty"),
-    
+    path('dashboard/',views.dashboard,name="dashboard"),
+    path('logout/',views.logout,name="logout"),
 
 ]
--- stayhubapp/views.py
+++ stayhubapp/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 def index(request):
-    return render(request,'index.html',)
-def loginn(request):
-    return render(request,'login.html',)
-def registration(request):
-    return render(request,'registration.html',)
-def registerproperty(request):
-    return render(request,'registerproperty.html',)
+    return render(request,'index.html')
 
+def loginn(request):
+    if request.method == "POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/dashboard')
+        else:
+            messages.info(request,"Invalid login")
+            return redirect('login')
+        
+    return render(request,'login.html')
+
+
+def registration(request):
+    if request.method == "POST":
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['username']
+        email=request.POST['email']
+        phone_number=request.POST['phone_number']
+        password=request.POST['password']
+        confirmPassword=request.POST['confirmPassword']
+        if User.objects.filter(username=username).exists():
+            messages.info(request,"Username already exists")
+            return redirect('registration')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request,"Email already exists")
+            return redirect('registration')
+        elif password != confirmPassword:
+            messages.error(request, "Password and confirmation password do not match")
+            return redirect('registration')
+        else:
+            user=User.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
+            user.save();
+        return redirect('login')
+    else:
+        return render(request,'registration.html')
+    
+def registerproperty(request):
+    return render(request,'registerproperty.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
